9fc66381790945b4d1ebb603695b372390b35b04
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-09-17 11:31:53 +1200

    Added code comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,56 @@
-var button = document.getElementById("enter")                   // Create button bariable called 'enter
-var input = document.getElementById("input")                    // Create input variable as 'input'
-var ul = document.querySelector("ul")                           // Create ul variable as 'ul'
+var button = document.getElementById("enter"); // Create button bariable called 'enter
+var input = document.getElementById("input"); // Create input variable as 'input'
+var ul = document.querySelector("ul"); // Create ul variable as 'ul'
 
-function inputLength() {                                        // Create function
-    return input.nodeValue.length;                              // Return the length of input
+function inputLength() {
+  // Create function
+  return input.nodeValue.length; // Return the length of input
 }
 
-function createListElemnet() {                                  // Create function
-    var li = document.createElement("li");                      // Create li variable as li element
-    li.appendChild(document.createTextNode(input.value));       // Append value of input variable to li variable
-    ul.appendChild(li);                                         // Append li variable to ul variable
+function createListElemnet() {
+  // Create function
+  var li = document.createElement("li"); // Create li variable as li element
+  li.appendChild(document.createTextNode(input.value)); // Append value of input variable to li variable
+  ul.appendChild(li); // Append li variable to ul variable
 
-    li.addEventListener("click", function () {                   // Create li event listener
-        var finished = this.classList.toggle("done")
-        var removeButton = document.createElement("button");     // Create button remove variable
-        removeButton.classList.add("deleteButton");              // Assign classlist to remove button variable
+  li.addEventListener("click", function () {
+    // Create li event listener
+    var finished = this.classList.toggle("done");
+    var removeButton = document.createElement("button"); // Create button remove variable
+    removeButton.classList.add("deleteButton"); // Assign classlist to remove button variable
 
-        if (finished) {                                                     // If finished
-            removeButton.appendChild(document.createTextNode("remove"))     // 
-            removeButton.classList = "deleteButton"
-            li.appendChild(removeButton)
-            removeButton.addEventListener("click", function () {
-                this.parentElement.remove()
-            })
-
-        } else {
-            this.getElementsByClassName("deleteButton")[0].remove();
-        }
-    });
-    input.value = "";
-}
-
-function addListAfterPress() {      // Create function
-    if (inputLength() > 0) {        // If lenegth is 0
-        createListElement();        // Create item list
+    if (finished) {
+      // If finished
+      removeButton.appendChild(document.createTextNode("remove")); // Insert remove text to the button
+      removeButton.classList = "deleteButton"; // Add class
+      li.appendChild(removeButton); // Add class to li element
+      removeButton.addEventListener("click", function () {
+        // add event to remove button variable
+        this.parentElement.remove(); // remove parent element
+      });
+    } else {
+      this.getElementsByClassName("deleteButton")[0].remove();
     }
+  });
+  input.value = "";
 }
 
-function addListAfterPress(event) {                         // Create event function
-    if (inputLength() > 0 && event.keyCode == 13) {         // If Input lengeht is greater 0 and event key code equals 13
-        createListElement();                                // Create a item list
-    }
+function addListAfterPress() {
+  // Create function
+  if (inputLength() > 0) {
+    // If lenegth is 0
+    createListElement(); // Create item list
+  }
 }
+
+function addListAfterPress(event) {
+  // Create event function
+  if (inputLength() > 0 && event.keyCode == 13) {
+    // If Input lengeht is greater 0 and event key code equals 13
+    createListElement(); // Create a item list
+  }
+}
+
+button.addEvenetListener("click", addListAfterClick);   // Create button
+
+input.addEventListener("keypress", addListAfterPress);  // Create input

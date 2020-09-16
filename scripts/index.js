838770982d414400067e6bf6f528cbdf9727b87e
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-09-17 11:27:46 +1200

    Changed JS file and added functions to it

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,44 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("input");
-var ul = document.querySelector("ul");
+var button = document.getElementById("enter")                   // Create button bariable called 'enter
+var input = document.getElementById("input")                    // Create input variable as 'input'
+var ul = document.querySelector("ul")                           // Create ul variable as 'ul'
 
-function inputLength() {
-    return input.value.length;
+function inputLength() {                                        // Create function
+    return input.nodeValue.length;                              // Return the length of input
 }
 
-function createListElement() {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
+function createListElemnet() {                                  // Create function
+    var li = document.createElement("li");                      // Create li variable as li element
+    li.appendChild(document.createTextNode(input.value));       // Append value of input variable to li variable
+    ul.appendChild(li);                                         // Append li variable to ul variable
+
+    li.addEventListener("click", function () {                   // Create li event listener
+        var finished = this.classList.toggle("done")
+        var removeButton = document.createElement("button");     // Create button remove variable
+        removeButton.classList.add("deleteButton");              // Assign classlist to remove button variable
+
+        if (finished) {                                                     // If finished
+            removeButton.appendChild(document.createTextNode("remove"))     // 
+            removeButton.classList = "deleteButton"
+            li.appendChild(removeButton)
+            removeButton.addEventListener("click", function () {
+                this.parentElement.remove()
+            })
+
+        } else {
+            this.getElementsByClassName("deleteButton")[0].remove();
+        }
+    });
+    input.value = "";
 }
 
-li.addEventListener("click", function() {
-    // creates a boolean that toggles the done class on li
-    // if the list item is clicked this toggles the done class
-    var finished = this.classList.toggle("done");
-    // creates a remove button for the finished item:
-    var removeButton = document.createElement("button");
-    removeButton.classList.add("deleteButton");
-
-    // if the list item is clicked (li and event listener) then
-    // finish is true
-    if (finished) {
-        removeButton.appendChild(document.createTextNode("remove"));
-        removeButton.classList = "deleteButton";
-        li.appendChild(removeButton);
-
-        removeButton.addEventListener("click",function() {
-            this.parentElement.remove();
-        });
-    } else {
-        this.getElementByClassName("deleteButton")[0].remove();
+function addListAfterPress() {      // Create function
+    if (inputLength() > 0) {        // If lenegth is 0
+        createListElement();        // Create item list
     }
-});
-// revert input value back to nothing
-input.value = "";
 }
 
-function addListAfterCick() {
-    if (inputLength() > 0) {
-        
+function addListAfterPress(event) {                         // Create event function
+    if (inputLength() > 0 && event.keyCode == 13) {         // If Input lengeht is greater 0 and event key code equals 13
+        createListElement();                                // Create a item list
     }
 }
